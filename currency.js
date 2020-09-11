458073d03090d7f7c5bd7ecb4093b9e43bc9d625
Alex Goodwin <hagoodwi@gmail.com>
2020-09-11 11:56:51 -0700

    add ui functionality

---
+++ currency.js
@@ -0,0 +1,25 @@
+export default class CurrencyExchange {  
+  static getExchange() {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true); 
+      request.send(); 
+    }); 
+
+    
+
+    
+  }
+  // getResult(){
+  //     if(country = "Argentina"){
+
+  //     }
+}
---
+++ src/main.js
@@ -0,0 +1,50 @@
+import $ from 'jquery'; 
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import CurrencyExchange from './currency';
+
+$(document).ready(function() {
+  $('#currencyExchange').click(function(){ 
+    let exchange = $('#currency').val();
+    let country = $('#country').val(); 
+    $('#formOne').val("");
+    $('#currency').val(""); 
+    let promise = CurrencyExchange.getExchange(country, exchange);
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      if(country == "Argentina"){
+        $('.showExchange').text(`The exchange rate of ${country} is ${body.conversion_rates.ARS}.`);
+        let result = `${exchange}`*`${body.conversion_rates.ARS}`;
+        $('.showResult').text(`The conversion of ${exchange} USD to ${country}'s currency is ${result}.`);
+      }
+      else if (country == "Australia"){
+        $('.showExchange').text(`The exchange rate of ${country} is ${body.conversion_rates.AUD}.`);
+        let result = `${exchange}`*`${body.conversion_rates.AUD}`;
+        $('.showResult').text(`The conversion of ${exchange} USD to ${country}'s currency is ${result}.`);
+      }
+      else if (country == "Bulgaria"){
+        $('.showExchange').text(`The exchange rate of ${country} is ${body.conversion_rates.BGN}.`);
+        let result = `${exchange}`*`${body.conversion_rates.BGN}`;
+        $('.showResult').text(`The conversion of ${exchange} USD to ${country}'s currency is ${result}.`);
+      }
+      else if (country == "Brazil"){
+        $('.showExchange').text(`The exchange rate of ${country} is ${body.conversion_rates.BRL}.`);
+        let result = `${exchange}`*`${body.conversion_rates.BRL}`;
+        $('.showResult').text(`The conversion of ${exchange} USD to ${country}'s currency is ${result}.`);
+      }
+      else if (country == "The Philippines"){
+        $('.showExchange').text(`The exchange rate of ${country} is ${body.conversion_rates.PHP}.`);
+        let result = `${exchange}`*`${body.conversion_rates.PHP}`;
+        $('.showResult').text(`The conversion of ${exchange} USD to ${country}'s currency is ${result}.`);
+      }
+      else{
+        $('.showExchange').text(`Typo, or ${country} is not supported.`);
+      }
+      
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+    });
+
+  });
+});
